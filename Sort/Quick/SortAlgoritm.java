255bbeebb767f1d7e55201af6fd5d2e98227a979
Noelsip <noelsipayung45@gmail.com>
2024-10-06 20:19:30 +0800

    add time

--- Sort/Quick/SortAlgoritm.java
+++ Sort/Quick/SortAlgoritm.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class SortAlgoritm {
     public static void main(String[] args) {
-        int []number = {11, 9, 4, 2, 4, 5, 4, 6, 8, 10, 1};
+        int []number = {7, 1, 5, 6, 3, 2,  4};
 
         System.out.println();
         System.out.println("== Sebelum Sorting ==");
@@ -12,11 +12,19 @@ public class SortAlgoritm {
 
         int ukuran = number.length;
         
-        quickSortAlgorithm(number, 0, ukuran-1);
+        long awal = System.currentTimeMillis();
 
+        quickSortAlgorithm(number, 0, ukuran-1);
+        
+        long akhir = System.currentTimeMillis();
+        
+        double waktu = akhir - awal ;
+        
         System.out.println("\n");
         System.out.println("== Setelah Sorting ==");
         System.out.println(Arrays.toString(number));
+
+        System.out.println("WAKTU SORTING: " + waktu + " ms");
 
     }
 
@@ -32,62 +40,62 @@ public class SortAlgoritm {
     // -----------------------------
 
     // Quicksort dengan pivot awal
-        int pivot = array[low];
-        int i = low;
-        int j = high;
+        // int pivot = array[low];
+        // int i = low;
+        // int j = high;
 
 
     // QuickSort dengan Pivot akhir
     // ---------------------------
-        // int pivot = array[high];
+        int pivot = array[high];
     // ---------------------------
 
     // Looping Pivot Awal
-    while (i < j){
-        while (array[j] > pivot) {
-            j--;
-        }
+    // while (i < j){
+    //     while (array[j] > pivot) {
+    //         j--;
+    //     }
 
-        while (i < j && array[i] <= pivot) {
-            i++;
-        }
+    //     while (i < j && array[i] <= pivot) {
+    //         i++;
+    //     }
 
-        if (j < i) {
-            int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+    //     if (j < i) {
+    //         int temp = array[i];
+    //         array[i] = array[j];
+    //         array[j] = temp;
 
-            System.out.println("Proses Patitioning: " + Arrays.toString(array));
-        }
-    }
+    //         System.out.println("Proses Patitioning: " + Arrays.toString(array));
+    //     }
+    // }
 
-    array[low] = array[j];
-    array[j] = pivot;
+    // array[low] = array[j];
+    // array[j] = pivot;
 
-    System.out.println("Proses Pivot Swap: " + Arrays.toString(array));
+    // System.out.println("Proses Pivot Swap: " + Arrays.toString(array));
 
-    return j;
+    // return j;
 
     // Looping Pivot Akhir
-        // int i = (low - 1);
+        int i = (low - 1);
 
-        // for (int j = low; j < high; j++) {
-        //     if (array[j] <= pivot) {
-        //         i++;
-        //         int temp = array[i];
-        //         array[i] = array[j];
-        //         array[j] = temp;
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
 
-        //         System.out.println("Partitioning Process: " + Arrays.toString(array));
-        //     }
-        // }
-        // int temp = array[i + 1];
-        // array[i + 1] = array[high];
-        // array[high] = temp;
+                System.out.println("Partitioning Process: " + Arrays.toString(array));
+            }
+        }
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
         
-        // System.out.println("Pivot Swap Process: " + Arrays.toString(array));
+        System.out.println("Pivot Swap Process: " + Arrays.toString(array));
 
-        // return (i + 1);
+        return (i + 1);
     }
 
     public static void quickSortAlgorithm(int []array, int low, int high){
